e056b57cd8dc410a6ee402e8a78269ceb63c1f08
gdomorski <gregdomorski@gmail.com>
2016-01-18 22:56:43 -0500

    Working on React stopwatch app

---
+++ stopwatch/index.ios.js
@@ -0,0 +1,35 @@
+var React = require('react-native');
+var {
+  Text,
+  View,
+  AppRegistry
+} = React;
+
+
+var Stopwatch = React.createClass({
+  render: function (){
+    return <View>
+      <Text>
+        00.00.00
+      </Text>
+      {this.startStopButton()}
+      {this.lapButton()}
+      </View>
+  },
+    startStopButton: function(){
+      return <View>
+        <Text>
+        Start
+        </Text>
+      </View>
+    },
+    lapButton: function(){
+      return <View>
+        <Text>
+          Lap
+        </Text>
+      </View>
+    }
+});
+
+AppRegistry.registerComponent('stopwatch', () => Stopwatch);
